d915c7a4f925625d743da0529f3957108bc9f1ed
daemonprocellae@live.it <daemonprocellae@live.it>
2022-03-08 17:53:07 +0100

    Aggiunto banner

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <TitleItem />
+    <HeroItem />
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import TitleItem from './components/TitleItem.vue'
+import HeroItem from './components/HeroItem.vue'
 
 export default {
   name: 'App',
   components: {
     // HelloWorld
-    TitleItem
+    TitleItem,
+    HeroItem
   }
 }
 
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap');
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/HeroItem.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="jumbo">
+
+        <div id="jumbo_title">
+            <h1>This is my hero banner!</h1>
+            <p>(fantasia portami via)</p>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+    #jumbo {
+        background-image: url("../assets/jumbo.png");
+        color: white;
+        text-align: center;
+        height: 500px;
+        font-family: 'Montserrat', sans-serif;
+
+        #jumbo_title {
+            position: absolute;
+            bottom: 10px;
+            right: 20px;
+        }
+
+        p {
+        font-size: 10px;
+        }
+    }   
+
+</style>
